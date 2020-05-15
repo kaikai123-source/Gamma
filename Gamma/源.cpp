a276658b99c91c56ad27dbe12867bef659cae5df
shengkai <Administrator@DESKTOP-539KRNO>
2020-05-15 13:31:42 +0800

    提交main

---
+++ Gamma/源.cpp
@@ -0,0 +1,64 @@
+#include<opencv2\opencv.hpp>
+#include<iostream>
+
+using namespace cv;
+using namespace std;
+
+
+void MyGammaCorrection(Mat& src, Mat& dst, float fGamma)
+{
+	CV_Assert(src.data);
+
+	// accept only uchar type matrices,when use CV_8UC1 or CV_8UC3,src.depth()==0
+	//sizeof(uchar)==1   
+	CV_Assert(src.depth() != sizeof(uchar));
+
+	// build look up table  
+	unsigned char lut[256];
+	for (int i = 0; i < 256; i++)
+	{
+		lut[i] = saturate_cast<uchar>(pow((float)(i / 255.0), fGamma) * 255.0f);
+	}
+
+	dst = src.clone();
+	const int channels = dst.channels();
+	switch (channels)
+	{
+	case 1:
+	{
+
+		MatIterator_<uchar> it, end;
+		for (it = dst.begin<uchar>(), end = dst.end<uchar>(); it != end; it++)
+			//*it = pow((float)(((*it))/255.0), fGamma) * 255.0;  
+			*it = lut[(*it)];
+
+		break;
+	}
+	case 3:
+	{
+
+		MatIterator_<Vec3b> it, end;
+		for (it = dst.begin<Vec3b>(), end = dst.end<Vec3b>(); it != end; it++)
+		{
+			//(*it)[0] = pow((float)(((*it)[0])/255.0), fGamma) * 255.0;  
+			//(*it)[1] = pow((float)(((*it)[1])/255.0), fGamma) * 255.0;  
+			//(*it)[2] = pow((float)(((*it)[2])/255.0), fGamma) * 255.0;  
+			(*it)[0] = lut[((*it)[0])];
+			(*it)[1] = lut[((*it)[1])];
+			(*it)[2] = lut[((*it)[2])];
+		}
+
+		break;
+
+	}
+	}
+}
+
+int main() {
+	Mat src,dst;
+	src = imread("D:\\face.jpg");
+	imshow("src", src);
+	MyGammaCorrection(src, dst, 1/2.2);
+	imshow("dts", dst);
+	waitKey(0);
+}
